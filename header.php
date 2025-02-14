acafc302732b0691da29f6eda15065c8aed7fdd1
FinanAI <yutu123xd@gmail.com>
2025-02-14 08:36:46 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('FinanAI'); ?></title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <?php wp_head(); ?>
+</head>
+<body>
+<header>
+    <h1><?php bloginfo('FinanAI'); ?></h1>
+</header>
